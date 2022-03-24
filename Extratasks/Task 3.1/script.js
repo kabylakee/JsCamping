5ad18d5207d74aae39ace2fbfb0a2e6615928989
Vika <vctrcute@gmail.com>
2022-03-24 16:18:07 +0300

    Fix extra 3.1

--- Extratasks/Task 3.1/script.js
+++ Extratasks/Task 3.1/script.js
@@ -1,5 +1,5 @@
 let searchMaxSum = (arr) => {
-let maxSum = 0;
+let maxSum = arr[0];
   for (let i = 0; i < arr.length; i++) {
     let tempSum = 0;
     for (let j = i; j < arr.length; j++) {
@@ -12,3 +12,4 @@ let maxSum = 0;
 
 console.log(searchMaxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4])); 
 console.log(searchMaxSum([5, 7, -10, 20, 30, -2, -1, 0, 3]));
+console.log(searchMaxSum([-1, -1, -1, -1, -1, -1, -1]));
